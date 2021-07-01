37588693edd153cb41858b05021998b5206c7a9a
andileni <andi.lenhardt@googlemail.com>
2021-07-01 11:05:26 +0200

    revert extension-point to fix image logging

--- boot.php
+++ boot.php
@@ -29,52 +29,53 @@ if (rex::isBackend() && rex_addon::get('dashboard')->isAvailable()) {
     );
 }
 
+
+
+// NOTICE: EP prevents media tracking
 // do actions after content is delivered
-rex_extension::register('RESPONSE_SHUTDOWN', function () {
-    // get ip from visitor, set to 0.0.0.0 when ip can not be determined
-    $whip = new Whip();
-    $clientAddress = $whip->getValidIpAddress();
-    $clientAddress = $clientAddress ? $clientAddress : '0.0.0.0';
+// rex_extension::register('RESPONSE_SHUTDOWN', function () {
+// });
+
+// get ip from visitor, set to 0.0.0.0 when ip can not be determined
+$whip = new Whip();
+$clientAddress = $whip->getValidIpAddress();
+$clientAddress = $clientAddress ? $clientAddress : '0.0.0.0';
+
+// page url
+$url = $_SERVER['REQUEST_URI'];
+
+// user agent
+$userAgent = $_SERVER['HTTP_USER_AGENT'];
+
+$visit = new stats_visit($clientAddress, $url, $userAgent);
 
 
-    // page url
-    $url = $_SERVER['REQUEST_URI'];
+// Track only frontend requests if page url should not be ignoredm
+if (!rex::isBackend() && !$visit->ignore_visit()) {
 
+    if ($visit->is_media()) {
 
-    // user agent
-    $userAgent = $_SERVER['HTTP_USER_AGENT'];
+        // request is a media request and should not be logged to the page-visits
+        $visit->save_media();
+    } else {
 
-    $visit = new stats_visit($clientAddress, $url, $userAgent);
+        // visit is not a media request, hence either bot or human visitor
 
+        // parse useragent
+        $visit->parse_ua();
 
+        if ($visit->DeviceDetector->isBot()) {
 
-    // Track only frontend requests if page url should not be ignoredm
-    if (!rex::isBackend() && !$visit->ignore_visit()) {
-
-        if ($visit->is_media()) {
-
-            // request is a media request and should not be logged to the page-visits
-            $visit->save_media();
+            // visitor is a bot
+            $visit->save_bot();
         } else {
 
-            // visit is not a media request, hence either bot or human visitor
+            if ($visit->save_visit()) {
 
-            // parse useragent
-            $visit->parse_ua();
-
-            if ($visit->DeviceDetector->isBot()) {
-
-                // visitor is a bot
-                $visit->save_bot();
-            } else {
-
-                if ($visit->save_visit()) {
-
-                    // visitor is human
-                    // check hash with save_visit, if true then save visit
-                    $visit->persist();
-                }
+                // visitor is human
+                // check hash with save_visit, if true then save visit
+                $visit->persist();
             }
         }
     }
-});
+}
