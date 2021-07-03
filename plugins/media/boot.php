0c1d50c05e7994108c30d718bab3db2f5c0a2f60
andileni <andi.lenhardt@googlemail.com>
2021-07-03 20:14:52 +0200

    optimization

--- plugins/media/boot.php
+++ plugins/media/boot.php
@@ -1,20 +1,23 @@
 <?php
 
-$plugin = rex_plugin::get('statistics', 'media');
+if (rex::isBackend()) {
 
-if (rex_addon::get('media_manager')->isAvailable()) {
-    rex_media_manager::addEffect(rex_effect_stats_mm::class);
-}
+    if (rex_addon::get('media_manager')->isAvailable()) {
+        rex_media_manager::addEffect(rex_effect_stats_mm::class);
+    }
+} else {
 
+    $plugin = rex_plugin::get('statistics', 'media');
+    
+    if ($plugin->getConfig('pagestats_media_log_all') == true) {
 
-if ($plugin->getConfig('pagestats_media_log_all') == true) {
+        $url = $_SERVER['REQUEST_URI'];
 
-    $url = $_SERVER['REQUEST_URI'];
+        $media_request = new stats_media_request($url);
 
-    $media_request = new stats_media_request($url);
+        if ($media_request->is_media()) {
 
-    if ($media_request->is_media()) {
-
-        $media_request->save_media();
+            $media_request->save_media();
+        }
     }
 }
