f7af666f2e5e67de1ec353e30a02f0ee7a5695ba
andileni <andi.lenhardt@googlemail.com>
2021-07-08 14:28:22 +0200

    add option to delete campaign

--- plugins/api/pages/campaigns.php
+++ plugins/api/pages/campaigns.php
@@ -1,12 +1,19 @@
 <?php
 
 $search_string = htmlspecialchars_decode(rex_request('search_string', 'string', ''));
-$request_name = rex_request('url', 'string', '');
+$request_name = rex_request('name', 'string', '');
 $request_name = htmlspecialchars_decode($request_name);
+$delete_entry = rex_request('delete_entry', 'boolean', false);
 
+
+if ($request_name != '' && $delete_entry === true) {
+    $sql = rex_sql::factory();
+    $sql->setQuery('delete from ' . rex::getTable('pagestats_api') . ' where name = :name', ['name' => $request_name]);
+    echo rex_view::success('Es wurden ' . $sql->getRows() . ' Einträge der Kampagne <code>' . $request_name . '</code> gelöscht.');
+}
 
 // details section
-if ($request_name != '') {
+if ($request_name != '' && !$delete_entry) {
     // details section for single campaign
 
     $pagedetails = new stats_campaign_details($request_name);
@@ -54,7 +61,12 @@ $list->setColumnLabel('name', $this->i18n('statistics_api_name'));
 $list->setColumnLabel('count', $this->i18n('statistics_api_count'));
 $list->setColumnSortable('name', $direction = 'asc');
 $list->setColumnSortable('count', $direction = 'asc');
-$list->setColumnParams('name', ['url' => '###name###']);
+$list->setColumnParams('name', ['name' => '###name###']);
+
+$list->addColumn('edit', $this->i18n('statistics_api_delete'));
+$list->setColumnLabel('edit', $this->i18n('statistics_api_delete'));
+$list->addLinkAttribute('edit', 'data-confirm', $this->i18n('statistics_api_delete_confirm'));
+$list->setColumnParams('edit', ['name' => '###name###', 'delete_entry' => true]);
 
 $fragment2 = new rex_fragment();
 $fragment2->setVar('title', $this->i18n('statistics_api_campaign_views'));
@@ -89,7 +101,7 @@ echo $fragment2->parse('core/page/section.php');
 
     <?php
 
-    if ($request_name != '') {
+    if ($request_name != '' && !$delete_entry) {
         echo 'chart_details = Plotly.newPlot("chart_details", [{
             type: "line",
             x:' . $sum_data['labels'] . ',
