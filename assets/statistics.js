9e28eea07da40a0a455393e643b2d6f8863cd967
Andreas Lenhardt <AndiLeni@users.noreply.github.com>
2021-11-03 18:46:53 +0100

    fix javascript error on datefilter quickselect / #66

--- assets/statistics.js
+++ assets/statistics.js
@@ -5,23 +5,25 @@ $(document).on("rex:ready", function (event, container) {
     statistics_df_ty = document.getElementById("statistics_df_ty");
     statistics_df_wt = document.getElementById("statistics_df_wt");
 
-    last_seven_days = get_past_date(7);
-    last_thirty_days = get_past_date(30);
-    this_year = get_this_year();
-    whole_time = statistics_df_wt.getAttribute("data-start");
-
-    statistics_df_lsd.addEventListener("click", function () {
-        statistics_datefilter_start.value = last_seven_days;
-    });
-    statistics_df_ltd.addEventListener("click", function () {
-        statistics_datefilter_start.value = last_thirty_days;
-    });
-    statistics_df_ty.addEventListener("click", function () {
-        statistics_datefilter_start.value = this_year;
-    });
-    statistics_df_wt.addEventListener("click", function () {
-        statistics_datefilter_start.value = whole_time;
-    });
+    // if input field exists add event listeners
+    if (statistics_datefilter_start != null) {
+        statistics_df_lsd.addEventListener("click", function () {
+            last_seven_days = get_past_date(7);
+            statistics_datefilter_start.value = last_seven_days;
+        });
+        statistics_df_ltd.addEventListener("click", function () {
+            last_thirty_days = get_past_date(30);
+            statistics_datefilter_start.value = last_thirty_days;
+        });
+        statistics_df_ty.addEventListener("click", function () {
+            this_year = get_this_year();
+            statistics_datefilter_start.value = this_year;
+        });
+        statistics_df_wt.addEventListener("click", function () {
+            whole_time = statistics_df_wt.getAttribute("data-start");
+            statistics_datefilter_start.value = whole_time;
+        });
+    }
 
     function get_past_date(minusDays) {
         var date = new Date();
