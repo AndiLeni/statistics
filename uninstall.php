a33a2d11f4fee46f6b83446fcb0fa988107ab666
andileni <andi.lenhardt@googlemail.com>
2021-12-12 11:19:45 +0100

    fix uninstall.php

--- uninstall.php
+++ uninstall.php
@@ -3,6 +3,7 @@
 rex_sql_table::get(rex::getTable('pagestats_dump'))->drop();
 rex_sql_table::get(rex::getTable('pagestats_data'))->drop();
 rex_sql_table::get(rex::getTable('pagestats_visits_per_day'))->drop();
+rex_sql_table::get(rex::getTable('pagestats_visitors_per_day'))->drop();
 rex_sql_table::get(rex::getTable('pagestats_visits_per_url'))->drop();
 rex_sql_table::get(rex::getTable('pagestats_bot'))->drop();
 rex_sql_table::get(rex::getTable('pagestats_hash'))->drop();
