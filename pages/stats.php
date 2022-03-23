a3d72de965b5076de39db4557e3fcb326b51d8e6
andileni <andi.lenhardt@googlemail.com>
2022-03-23 10:46:51 +0100

    better handling of null values from db

--- pages/stats.php
+++ pages/stats.php
@@ -181,40 +181,25 @@ if (count($domains) > 1) {
 
 // FRAGMENT TO SHOW TODAYS AND TOTAL COUNT OF VIEWS
 
-$visits_total = $sql->setQuery('SELECT sum(count) as "count" from ' . rex::getTable('pagestats_visits_per_day'));
+$visits_total = $sql->setQuery('SELECT ifnull(sum(count),0) as "count" from ' . rex::getTable('pagestats_visits_per_day'));
 $visits_total = $visits_total->getValue('count');
 
-$visits_today = $sql->setQuery('SELECT sum(count) as "count" from ' . rex::getTable('pagestats_visits_per_day') . ' where date = :date', ['date' => date('Y-m-d')]);
-if ($visits_today->getRows() != 0) {
-    $visits_today = $visits_today->getValue('count');
-} else {
-    $visits_today = 0;
-}
+$visits_today = $sql->setQuery('SELECT ifnull(sum(count),0) as "count" from ' . rex::getTable('pagestats_visits_per_day') . ' where date = :date', ['date' => date('Y-m-d')]);
+$visits_today = $visits_today->getValue('count');
 
-$visitors_total = $sql->setQuery('SELECT sum(count) as "count" from ' . rex::getTable('pagestats_visitors_per_day'));
+$visitors_total = $sql->setQuery('SELECT ifnull(sum(count),0) as "count" from ' . rex::getTable('pagestats_visitors_per_day'));
 $visitors_total = $visitors_total->getValue('count');
 
-$visitors_today = $sql->setQuery('SELECT count from ' . rex::getTable('pagestats_visitors_per_day') . ' where date = :date', ['date' => date('Y-m-d')]);
-if ($visitors_today->getRows() != 0) {
-    $visitors_today = $visitors_today->getValue('count');
-} else {
-    $visitors_today = 0;
-}
+$visitors_today = $sql->setQuery('SELECT ifnull(sum(count),0) as "count" from ' . rex::getTable('pagestats_visitors_per_day') . ' where date = :date', ['date' => date('Y-m-d')]);
+$visitors_today = $visitors_today->getValue('count');
 
 
-$visits_datefilter = $sql->setQuery('SELECT sum(count) as "count" from ' . rex::getTable('pagestats_visits_per_day') . ' where date between :start and :end', ['start' => $filter_date_helper->date_start->format('Y-m-d'), ':end' => $filter_date_helper->date_end->format('Y-m-d')]);
-if ($visits_datefilter->getRows() != 0) {
-    $visits_datefilter = $visits_datefilter->getValue('count');
-} else {
-    $visits_datefilter = 0;
-}
+$visits_datefilter = $sql->setQuery('SELECT ifnull(sum(count),0) as "count" from ' . rex::getTable('pagestats_visits_per_day') . ' where date between :start and :end', ['start' => $filter_date_helper->date_start->format('Y-m-d'), ':end' => $filter_date_helper->date_end->format('Y-m-d')]);
+$visits_datefilter = $visits_datefilter->getValue('count');
 
-$visitors_datefilter = $sql->setQuery('SELECT sum(count) as "count" from ' . rex::getTable('pagestats_visitors_per_day') . ' where date between :start and :end', ['start' => $filter_date_helper->date_start->format('Y-m-d'), ':end' => $filter_date_helper->date_end->format('Y-m-d')]);
-if ($visitors_datefilter->getRows() != 0) {
-    $visitors_datefilter = $visitors_datefilter->getValue('count');
-} else {
-    $visitors_datefilter = 0;
-}
+$visitors_datefilter = $sql->setQuery('SELECT ifnull(sum(count),0) as "count" from ' . rex::getTable('pagestats_visitors_per_day') . ' where date between :start and :end', ['start' => $filter_date_helper->date_start->format('Y-m-d'), ':end' => $filter_date_helper->date_end->format('Y-m-d')]);
+$visitors_datefilter = $visitors_datefilter->getValue('count');
+
 
 $fragment_overview = new rex_fragment();
 $fragment_overview->setVar('date_start', $filter_date_helper->date_start);
