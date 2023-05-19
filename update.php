30a9d3a3224842888bc4e81e42861d4817e1fd3f
Andreas Lenhardt <andi.lenhardt@googlemail.com>
2023-05-19 18:55:20 +0200

    3.0.0beta3

--- update.php
+++ update.php
@@ -33,7 +33,9 @@ foreach ($data as $e) {
 if (rex_plugin::exists('statistics', 'api')) {
 
     // copy old config
-    rex_config::set("statistics_api_enable", rex_config::get("statistics/api", "statistics_api_enable"));
+    if (rex_config::has("statistics/api", "statistics_api_enable")) {
+        rex_config::set("statistics_api_enable", rex_config::get("statistics/api", "statistics_api_enable"));
+    }
 
     // delete uninstall.php of plugin
     rex_file::delete(rex_path::plugin('statistics', 'api', 'uninstall.php'));
@@ -44,8 +46,13 @@ if (rex_plugin::exists('statistics', 'api')) {
 }
 
 if (rex_plugin::exists('statistics', 'media')) {
-    rex_config::set("statistics_media_log_all", rex_config::get("statistics/media", "statistics_media_log_all"));
-    rex_config::set("statistics_media_log_mm", rex_config::get("statistics/media", "statistics_media_log_mm"));
+
+    if (rex_config::has("statistics/media", "statistics_media_log_all")) {
+        rex_config::set("statistics_media_log_all", rex_config::get("statistics/media", "statistics_media_log_all"));
+    }
+    if (rex_config::has("statistics/media", "statistics_media_log_mm")) {
+        rex_config::set("statistics_media_log_mm", rex_config::get("statistics/media", "statistics_media_log_mm"));
+    }
 
     rex_file::delete(rex_path::plugin('statistics', 'media', 'uninstall.php'));
 
