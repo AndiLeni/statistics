33b7ca7634c3e8b4c863816a93da004809603742
Andreas Lenhardt <andi.lenhardt@googlemail.com>
2023-05-19 20:09:23 +0200

    remove old v1->v2 update migrations

--- update.php
+++ update.php
@@ -2,32 +2,8 @@
 
 $addon = rex_addon::get('statistics');
 
-// fix table rex_pagestats_referer
 
-$sql = rex_sql::factory();
-$query = '
-select referer, date, count(count) as "count"
-from ' . rex::getTable('pagestats_referer') . '
-group by referer, date;
-';
-$data = $sql->getArray($query);
-
-
-// delete old table
-rex_sql_table::get(rex::getTable('pagestats_referer'))->drop();
-
-// create new tables
-$addon->includeFile(__DIR__ . '/install.php');
-
-foreach ($data as $e) {
-    $sql_insert = 'INSERT INTO ' . rex::getTable('pagestats_referer') . ' (hash,referer,date,count) VALUES 
-    ("' . md5($e['date'] . $e['referer']) . '","' . addslashes($e['referer']) . '","' . $e['date'] . '",' . $e['count'] . ');';
-
-    $res = $sql->setQuery($sql_insert);
-}
-
-
-// version 3
+// version 3 migrations
 
 // remove old plugins 
 if (rex_plugin::exists('statistics', 'api')) {
