4637da091d2ad2790eae00a90b26240b3fd18470
Andreas Lenhardt <andi.lenhardt@googlemail.com>
2023-05-19 18:27:25 +0200

    update update.php

--- update.php
+++ update.php
@@ -25,3 +25,31 @@ foreach ($data as $e) {
 
     $res = $sql->setQuery($sql_insert);
 }
+
+
+// version 3
+
+// remove old plugins 
+if (rex_plugin::exists('statistics', 'api')) {
+
+    // copy old config
+    rex_config::set("statistics_api_enable", rex_config::get("statistics/api", "statistics_api_enable"));
+
+    // delete uninstall.php of plugin
+    rex_file::delete(rex_path::plugin('statistics', 'api', 'uninstall.php'));
+
+    $pl_api = rex_package::get('statistics', 'api');
+    $pl_api_manager = rex_package_manager::factory($pl_api);
+    $pl_api_manager->delete();
+}
+
+if (rex_plugin::exists('statistics', 'media')) {
+    rex_config::set("statistics_media_log_all", rex_config::get("statistics/media", "statistics_media_log_all"));
+    rex_config::set("statistics_media_log_mm", rex_config::get("statistics/media", "statistics_media_log_mm"));
+
+    rex_file::delete(rex_path::plugin('statistics', 'media', 'uninstall.php'));
+
+    $pl_api = rex_package::get('statistics', 'media');
+    $pl_api_manager = rex_package_manager::factory($pl_api);
+    $pl_api_manager->delete();
+}
